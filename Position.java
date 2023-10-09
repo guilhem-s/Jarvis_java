4b8c1483467f0b81da19bff2ed6a4d0513ffd6f2
estellelm38 <144695298+estellelm38@users.noreply.github.com>
2023-10-09 15:48:03 +0200

    Create Position.java

---
+++ Position.java
@@ -0,0 +1,12 @@
+public class Position() {
+  Wheel roue1 = WheeledChassis.modeWheel((RegulatorMotor) Motor.B,diametre).offset(70);
+  Wheel roue2 = WheeledChassis.modeWheel((RegulatorMotor) Motor.A,diametre).offset(-70);
+  Chassis chassis = new WheeledChassis(new Wheel[] {roue1, roue2}), WheeledChassis.TYPE_DIFFERENTIAL);
+  MovePilot pilot = new MovePilot(chassis);
+  int x=10; //Constante à définir en testant
+
+  public static void perpendiculaire(){
+    pilot.travel(x);
+  }
+}
+    
