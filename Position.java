1652ae8fafd34adf5da96a7e3883091e0a149fc9
estellelm38 <144695298+estellelm38@users.noreply.github.com>
2023-10-09 15:49:49 +0200

    Update Position.java

--- Position.java
+++ Position.java
@@ -1,3 +1,5 @@
+package ev3dev.sensors.ev3;
+import USSensorDemo;
 public class Position() {
   Wheel roue1 = WheeledChassis.modeWheel((RegulatorMotor) Motor.B,diametre).offset(70);
   Wheel roue2 = WheeledChassis.modeWheel((RegulatorMotor) Motor.A,diametre).offset(-70);
