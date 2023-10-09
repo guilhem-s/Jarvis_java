80ad39ec97942f587fbe9766f5e265a5ff70119d
estellelm38 <144695298+estellelm38@users.noreply.github.com>
2023-10-09 16:28:39 +0200

    Update Position.java

--- Position.java
+++ Position.java
@@ -1,14 +1,20 @@
 package ev3dev.sensors.ev3;
+import lejos.hardware.port.SensorPort;
 import USSensorDemo;
 public class Position() {
   Wheel roue1 = WheeledChassis.modeWheel((RegulatorMotor) Motor.B,diametre).offset(70);
   Wheel roue2 = WheeledChassis.modeWheel((RegulatorMotor) Motor.A,diametre).offset(-70);
   Chassis chassis = new WheeledChassis(new Wheel[] {roue1, roue2}), WheeledChassis.TYPE_DIFFERENTIAL);
   MovePilot pilot = new MovePilot(chassis);
+  USSensorDemo ultraSensor;
   int x=10; //Constante à définir en testant
 
   public static void perpendiculaire(){
     pilot.travel(x);
+    float[] distance = new float[5];
+    distance = ultraSensor.getDistance(); // pas sur que ce soit cette fonction
+    pilot.travel(2*x);
+    
   }
 }
     
