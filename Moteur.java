831191b7ff075bb45ae9ba61c55b2de2d479dcc3
guilhem123Z <65088860+guilhem123Z@users.noreply.github.com>
2023-10-16 15:33:41 +0200

    Update Moteur.java

--- Moteur.java
+++ Moteur.java
@@ -1,4 +1,38 @@
-package 
+package S5;
+
+import lejos.hardware.BrickFinder;
+import lejos.hardware.lcd.GraphicsLCD;
+import lejos.hardware.motor.Motor;
+import lejos.utility.Delay;
+import lejos.hardware.port.MotorPort;
+import lejos.hardware.port.Port;
 import lejos.hardware.port.SensorPort;
+import lejos.robotics.RegulatedMotor;
+import lejos.robotics.chassis.Wheel;
+import lejos.robotics.chassis.WheeledChassis;
+import lejos.robotics.navigation.MovePilot;
+import lejos.robotics.chassis.*;
 
-
+public class Moteur {
+	
+	private static final int RAPIDE = 90;
+	private static final int LENT = 45;
+	private static final int RECHERCHE = 30;
+	private static final double DIAMETRE = 2.2;
+	private MovePilot pilot;
+	
+	public Moteur () {
+		Wheel roue1 = WheeledChassis.modelWheel((RegulatedMotor)Motor.B,DIAMETRE ).offset(-70);
+		Wheel roue2 = WheeledChassis.modelWheel((RegulatedMotor)Motor.A, DIAMETRE).offset(70);
+		Chassis chassis = new WheeledChassis(new Wheel[] {roue1, roue2}, WheeledChassis.TYPE_DIFFERENTIAL);
+		pilot = new MovePilot(chassis);
+	}
+	
+	public MovePilot pilote() {
+		return pilot;
+	}
+	
+	public void tourner(int angle) {
+		pilot.rotate(angle);		
+	}
+}
