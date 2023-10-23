41ca1bd4c88f6b7c10c5dd7b665fc9e4ed2332c7
estellelm38 <144695298+estellelm38@users.noreply.github.com>
2023-10-23 15:03:09 +0200

    Update Fonctionnalite.java

--- Fonctionnalite.java
+++ Fonctionnalite.java
@@ -1,25 +1,26 @@
 package S5;
 
+import lejos.hardware.Button;
 import lejos.utility.Delay;
 
 public class Fonctionnalite {
 	Moteur m;
 	Capteur c;
 
-	
+
 	public Fonctionnalite() {
 		m = new Moteur();
 		c = new Capteur();
 	}
-	
+
 	public void perpendiculaire_apres_essai() {
 		int d1, d2;
-		m.viteseRot(5);
+		m.changerVitRot(5);
 		d1 = c.echantillon();		
 		m.tourner(5);
 		Delay.msDelay(500);
 		d2 = c.echantillon();
-		
+
 		if (d1 < d2) {
 			m.tourner(-10);
 			while (d1 > d2) {
@@ -29,7 +30,7 @@ public class Fonctionnalite {
 				d2 = c.echantillon();
 			}
 			m.tourner(5);
-			
+
 		} else {
 			while (d1 > d2) {
 				m.tourner(5);
@@ -37,12 +38,29 @@ public class Fonctionnalite {
 				Delay.msDelay(100);
 				d2 = c.echantillon();
 			}
-		m.tourner(-5);
+			m.tourner(-5);
+		}
+	}
+
+	public void detectionPalet(float seuilDetection) {
+		while (true) {
+			float distanceDetectee = c.echantillon();
+			if (distanceDetectee < seuilDetection) {
+				System.out.println("palet détecté");
+				while (!Button.ENTER.isDown()) {
+					if (c.echantillon() == 1.00) {
+						m.fermerPinces(1600);
+						break;
+					}
+
+				}
+				Delay.msDelay(500);
+			}
 		}
 	}
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		Moteur m = new Moteur();
+		//Moteur m = new Moteur();
 	}
 
 }
