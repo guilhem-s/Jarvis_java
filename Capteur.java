9a7ffae8899cf07d881b10bffc5596f7e0aa4b48
estellelm38 <144695298+estellelm38@users.noreply.github.com>
2023-11-12 12:06:47 +0100

    Update Capteur.java

--- Capteur.java
+++ Capteur.java
@@ -1,5 +1,6 @@
 package S5;
 
+import lejos.hardware.Button;
 import lejos.hardware.port.SensorPort;
 import lejos.hardware.sensor.EV3ColorSensor;
 import lejos.hardware.sensor.EV3TouchSensor;
@@ -8,37 +9,56 @@ import lejos.robotics.SampleProvider;
 import lejos.utility.Delay;
 
 public class Capteur {
-	
+
 	private EV3UltrasonicSensor ultrason;
 	private EV3TouchSensor touche;
 	private EV3ColorSensor couleur;
+	Moteur m;
 
 	public Capteur (){
 		ultrason = new EV3UltrasonicSensor(SensorPort.S4);
 		touche = new EV3TouchSensor(SensorPort.S3);
 		couleur = new EV3ColorSensor(SensorPort.S2);
-		
+
 	}
 	public int echantillon() {
 		final SampleProvider sp = ultrason.getDistanceMode();
 		float[] sample = new float[sp.sampleSize()];
 		sp.fetchSample(sample, 0);
-		return (int) sample[0];
+		float result = sample[0]*1000;
+		return  (int) result;
 	}
 
 
 	public float[] getSample(int seuil){
-		final SampleProvider sp = ultraSensor.getDistanceMode();
-		int distanceValue = 0;
-		float []samples = new float [seuil];
+		final SampleProvider sp = ultrason.getDistanceMode();
+		float[] distanceValue = new float[seuil];
 		for(int i = 0; i < seuil; i++){
 			float[] sample = new float[sp.sampleSize()];
 			sp.fetchSample(sample, 0);
-			distanceValue = (int)sample[0];
-			samples[i] = distanceValue;
+			distanceValue[i] = sample[0];
+			System.out.println("Valeur : " + i + "Distance : " + sample[0]);
 			Delay.msDelay(500);
 		}
-		return samples;  
+		return distanceValue;  // A changer
 	}
-	
+
+	public void vers_ligne_arrivee() {
+		if(! couleur.isFloodlightOn())  // teste si la lampe est allumée
+			couleur.setFloodlight(true);
+		while (true) {
+			m.avancerSync(100);
+			int colorID = couleur.getColorID();
+			if (colorID == 6) break;
+			try {
+				Thread.sleep(250); // Attendre 0,25 seconde entre les mesures
+			} catch (InterruptedException e) {
+				e.printStackTrace();
+			}
+			if(Button.ENTER.isDown())break;
+		}
+		m.stop();
+		couleur.close();
+	}
+
 }
