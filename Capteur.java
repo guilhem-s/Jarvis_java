bfef48170d79abf3d2e252d2491f6bd2b49bd456
guilhem123Z <65088860+guilhem123Z@users.noreply.github.com>
2023-11-29 16:29:27 +0100

    Update Capteur.java

--- Capteur.java
+++ Capteur.java
@@ -1,5 +1,6 @@
 package S5;
 
+import java.util.ArrayList;
 import lejos.hardware.port.SensorPort;
 import lejos.hardware.sensor.EV3ColorSensor;
 import lejos.hardware.sensor.EV3TouchSensor;
@@ -11,47 +12,45 @@ public class Capteur {
 
 	private EV3UltrasonicSensor ultrason;
 	private EV3TouchSensor toucher;
-	private EV3ColorSensor capcouleur;
-	Moteur m;
+	private EV3ColorSensor couleur;
 
 	public Capteur (){
 		ultrason = new EV3UltrasonicSensor(SensorPort.S4);
 		toucher = new EV3TouchSensor(SensorPort.S3);
-		capcouleur = new EV3ColorSensor(SensorPort.S2);
+		couleur = new EV3ColorSensor(SensorPort.S2);
 
 	}
-	public int echantillon() {
-		final SampleProvider sp = ultrason.getDistanceMode();
-		float[] sample = new float[sp.sampleSize()];
-		sp.fetchSample(sample, 0);
-		float result = sample[0]*100;
-		return  (int) result;
-	}
+	public EV3TouchSensor getTouche() { return toucher;	}
+	public EV3UltrasonicSensor getUltrason() { return ultrason;	}
+	public EV3ColorSensor getCouleur() { return couleur; }
 
-
-	public float[] getSample(int seuil){
+	public int[] getSample(int seuil, int delay){
 		final SampleProvider sp = ultrason.getDistanceMode();
-		float[] distanceValue = new float[seuil];
+		int[] distanceValue = new int[seuil];
 		for(int i = 0; i < seuil; i++){
 			float[] sample = new float[sp.sampleSize()];
 			sp.fetchSample(sample, 0);
-			distanceValue[i] = sample[0];
-			System.out.println("Valeur : " + i + "Distance : " + sample[0]);
-			Delay.msDelay(500);
+			sample[0] = sample[0]>300?300:sample[0];
+			System.out.println((int) (sample[0]*100));
+			distanceValue[i] = (int) (sample[0]*100);
+			Delay.msDelay(delay);
 		}
 		return distanceValue;  // A changer
 	}
 
-	
-	public EV3TouchSensor getTouche() {
-		// TODO Auto-generated method stub
-		return toucher;
+	public ArrayList<Integer> echantillon_en_tournant(Moteur m, int angle_de_balayage) {
+		int i = 0;
+		final SampleProvider sp = ultrason.getDistanceMode();
+		float[] sample = new float[sp.sampleSize()];
+		ArrayList<Integer> distanceValue = new ArrayList<Integer>();
+		m.tournerSync(angle_de_balayage);
+		while(m.isMoving()) {
+			sp.fetchSample(sample, 0); 
+			int echantillon = sample[0]>2.5?250:(int) (sample[0]*100); // Au delà de 250, les mesures n'ont pas d'utilité pour nous
+			distanceValue.add(echantillon); // echantillon est entier et converti en centimètres 
+			System.out.println(i+"- "+distanceValue.get(i));
+			i++;			
+		}
+		return distanceValue;
 	}
-	public EV3UltrasonicSensor getUltrason() {
-		return ultrason;
-	}
-	public EV3ColorSensor getCapcouleur() {
-		return capcouleur;
-	}
-
 }
