7115de7950493620a11817fd471b697baf2ab259
guilhem123Z <65088860+guilhem123Z@users.noreply.github.com>
2023-11-29 11:35:48 +0100

    Create Surveillance.java

---
+++ Surveillance.java
@@ -0,0 +1,36 @@
+package S5;
+
+import lejos.hardware.Button;
+
+public class Surveillance {
+    private Thread surveillanceBoutonThread;
+
+    // Méthode pour lancer la surveillance du bouton
+    public void lancerSurveillanceBouton() {
+        surveillanceBoutonThread = new Thread(new Runnable() {
+            @Override
+            public void run() {
+                while (Fonctionnalite.enCours) {
+                    if (Button.ESCAPE.isDown()) {
+                        Fonctionnalite.enCours = false;
+                        System.exit(0);}
+                    // Pause courte
+                    try {
+                        Thread.sleep(500);
+                    } catch (InterruptedException e) {
+                    	e.printStackTrace();
+                    }
+                }}});
+        surveillanceBoutonThread.start();
+    }
+
+    // Méthode pour attendre que les threads de surveillance se terminent
+    public void attendreFinSurveillance() {
+        try {
+            if (surveillanceBoutonThread != null) {
+                surveillanceBoutonThread.join();
+            }
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }}
+}
